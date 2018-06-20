2208df538aede34469883463db95464ad11e6f29
EPASYSTEM <epasystem@live.jp>
2018-06-20 17:05:40 +0900

    first commit

---
+++ pages/callback.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container d-flex align-items-center justify-content-center flex-column">
+    <v-progress-circular :size="70" :width="7" indeterminate color="green"></v-progress-circular>
+    Logging in...
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 70vh;
+}
+</style>
